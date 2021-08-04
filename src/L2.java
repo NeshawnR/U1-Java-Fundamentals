d291a180761e64c7dddd60b55c1d3e6bb62be362
richane000 <richane000@warren.k12.in.us>
2021-08-04 12:51:53 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,22 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args){
+        double num1 = 3.4;
+        boolean num2 = true;
+        double num3 = 4.5;
+
+       String firstName = "George";
+       int age = 16;
+        System.out.println("firstName");
+        System.out.println(age);
+
+        String accountName = "Neshawn";
+        int accountNumber = 123456789;
+        double balance = 123456789;
+
+        System.out.println(accountName);
+        System.out.println(accountNumber);
+        System.out.println(balance);
+    }
 }
